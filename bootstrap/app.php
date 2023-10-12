eba7ccb8983a669c5aef06691c7aea93903a16d0
chipslays <gorshkov34@gmail.com>
2023-10-12 09:20:52 +0400

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,11 @@
 
 use Alisa\Skill;
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+require __DIR__ . '/database.php';
+
 $config = require __DIR__ . '/../config/app.php';
 $config['assets'] = require __DIR__ . '/../config/assets.php';
 $config['middlewares'] = require __DIR__ . '/../config/middlewares.php';
